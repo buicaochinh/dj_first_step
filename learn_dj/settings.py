c7d557ae7b9bcbbbe3352a022aafc4f89dadf049
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2021-03-14 06:04:25 +0700

    part 7: done | basic all done

--- learn_dj/settings.py
+++ learn_dj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'learn_dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  fieldsets = [
+      ('Question Text',               {'fields': ['question_text']}),
+      ('Date information', {'fields': ['pub_date']}),
+  ]
+  inlines = [ChoiceInline]
+  list_filter = ['pub_date']
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
